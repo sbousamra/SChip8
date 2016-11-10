e4df0aa4358b14c3c53fec6df9f311fc40361605
Bass <sebastian.bousamra@hotmail.com>
2016-11-10 13:38:46 +1000

    latest commit

--- Memory.scala
+++ Memory.scala
@@ -7,12 +7,12 @@ case class Memory(data: List[Byte])
 
 object Memory {
 
-  val availableMemory = List.fill(0, 4096)(0)
+  val availableMemory: List[Byte] = List.fill(4096)(0.toByte)
 
   def loadRom(romPath: String): List[Byte] = {
     val pathToFile = new File(romPath)
-    val romSource = Source.fromFile(pathToFile).toList.map(_.toByte)
-    val romData = availableMemory
-    romSource
+    val romToBytes = Source.fromFile(pathToFile)(Codec.ISO8859).toList.map(_.toByte)
+    val loadBytesToMemory = availableMemory.patch(0x200, romToBytes, romToBytes.length)
+    loadBytesToMemory
   }
 }
